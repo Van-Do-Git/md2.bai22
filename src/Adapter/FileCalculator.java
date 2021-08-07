7e3e197f6ede49eda14c50b93ef6fc6fac70fc2e
unknown <mittervan@gmail.com>
2021-08-07 14:24:06 +0700

    Practice Subtructral pattern

---
+++ src/Adapter/FileCalculator.java
@@ -0,0 +1,5 @@
+package Adapter;
+
+public interface FileCalculator {
+    long calculateSize(String path);
+}
---
+++ src/Adapter/FileUtil.java
@@ -0,0 +1,9 @@
+package Adapter;
+
+import java.io.File;
+
+public class FileUtil {
+    public long calculateSize(File file) {
+        return 1;
+    }
+}
